47ead247860be52c889ab0ddcebe76c51e5844e9
NagayamaRyoga <nagayama15@sec.is.kit.ac.jp>
2021-03-04 22:05:45 +0900

    add a markdown converter

---
+++ bin/buildmd.js
@@ -0,0 +1,29 @@
+const processmd = require("processmd").default;
+
+const options = {
+  files: `${__dirname}/../posts/*.md`,
+  outputDir: `${__dirname}/../dist`,
+  summaryOutput: `${__dirname}/../dist/summary.json`,
+  filenamePrefix: "",
+  renderLatex: true,
+  highlightCode: true,
+  preview: 140,
+  previewDelimiter: "\n",
+  includeBodyProps: false,
+  includeTitle: true,
+  includeDir: false,
+  includeBase: false,
+  includeExt: false,
+  includeSourceBase: true,
+  includeSourceExt: false,
+  convertMode: "json",
+  stdout: false,
+  markdownRenderer: null,
+  markdownOptions: {
+    html: true,
+    linkify: true,
+  },
+  headingIds: false,
+};
+
+processmd(options);
