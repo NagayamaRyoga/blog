b4adcf40fcbd0897b52682b1c6c1e6abe47a6d2d
NagayamaRyoga <nagayama15@sec.is.kit.ac.jp>
2022-05-28 21:01:28 +0900

    fix: fix warning

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Head from "next/head";
 import { ThemeProvider } from "@emotion/react";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
@@ -9,10 +8,6 @@ import { defaultTheme } from "@/styles/theme";
 // eslint-disable-next-line react/prop-types
 const App = ({ Component, pageProps }) => (
   <>
-    <Head>
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;500&display=swap" />
-    </Head>
     <ThemeProvider theme={defaultTheme}>
       {globalStyles}
       <Component {...pageProps} />
